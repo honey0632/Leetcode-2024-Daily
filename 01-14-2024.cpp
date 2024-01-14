e1f463b5174f4fc545647f69f2787f48048b5c2b
honey0632 <honey603020@gmail.com>
2024-01-14 14:42:57 +0530

    jan 14

---
+++ 01-14-2024.cpp
@@ -0,0 +1,43 @@
+// 1657. Determine if Two Strings Are Close
+// https://leetcode.com/problems/determine-if-two-strings-are-close/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
+
+        for (char ch : word1) {
+            freq1[ch - 'a']++;
+        }
+
+        for (char ch : word2) {
+            freq2[ch - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main () {
+  
+   return 0;
+}
