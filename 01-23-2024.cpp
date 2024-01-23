e7d5091d3b61b8f65efabb915e2199a17c61a1e1
honey0632 <honey603020@gmail.com>
2024-01-23 21:56:37 +0530

    jan 23

---
+++ 01-23-2024.cpp
@@ -0,0 +1,46 @@
+// 1239. Maximum Length of a Concatenated String with Unique Characters
+// https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxLength(vector<string>& arr) {
+        int result = 0;
+        dfs(arr, "", 0, result);
+        return result;
+    }
+
+private:
+    void dfs(const vector<string>& arr, const string& path, int idx, int& result) {
+        if (isUniqueChars(path)) {
+            result = max(result, static_cast<int>(path.length()));
+        }
+
+        if (idx == arr.size() || !isUniqueChars(path)) {
+            return;
+        }
+
+        for (int i = idx; i < arr.size(); i++) {
+            dfs(arr, path + arr[i], i + 1, result);
+        }
+    }
+
+    bool isUniqueChars(const string& s) {
+        unordered_set<char> set;
+        for (char c : s) {
+            if (set.count(c)) {
+                return false;
+            }
+            set.insert(c);
+        }
+        return true;
+    }
+};
+
+int main () {
+  
+   return 0;
+}
