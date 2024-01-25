f3d6d5562ebf60c567a40b1bb5efdd1179b8e98a
honey0632 <honey603020@gmail.com>
2024-01-25 21:47:37 +0530

    jan 25

---
+++ 01-25-2024.cpp
@@ -0,0 +1,33 @@
+// 1143. Longest Common Subsequence
+// https://leetcode.com/problems/longest-common-subsequence/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Function to find the length of the longest common subsequence in two strings.
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                      dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+      
+        return dp[n][m];
+    }
+};
+
+int main () {
+  
+   return 0;
+}
