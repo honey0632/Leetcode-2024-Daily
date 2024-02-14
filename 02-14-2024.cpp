79f7b2f3dc6cb019e80b1cfd87dba30607291016
honey0632 <honey603020@gmail.com>
2024-02-14 19:08:40 +0530

    feb 14

---
+++ 02-14-2024.cpp
@@ -0,0 +1,37 @@
+// 2149. Rearrange Array Elements by Sign
+// https://leetcode.com/problems/rearrange-array-elements-by-sign/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> rearrangeArray(vector<int> &nums)
+  {
+    vector<int> ans(nums.size(), 0);
+
+    int pos = 0, neg = 1;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if (nums[i] > 0)
+      {
+        ans[pos] = nums[i];
+        pos += 2;
+      }
+      else
+      {
+        ans[neg] = nums[i];
+        neg += 2;
+      }
+    }
+    return ans;
+  }
+};
+
+int main()
+{
+
+  return 0;
+}
