07a4c9efa77cb28832ade6530bacff123ca21eb7
honey0632 <honey603020@gmail.com>
2024-03-10 13:31:04 +0530

    mar 10

---
+++ 03-10-2024.cpp
@@ -0,0 +1,29 @@
+// 349. Intersection of Two Arrays
+// https://leetcode.com/problems/intersection-of-two-arrays/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+                int m = nums2.size();
+        vector<int>result;
+        unordered_map<int,int>mp;
+        for(auto &num : nums1){
+            mp[num]++;
+        }
+        for(auto & num : nums2){
+            if(mp.find(num) != mp.end()){
+                result.push_back(num);
+                mp.erase(num);
+            }
+        }
+        return result;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
