9fa7ad5ac38ae4b5e2d7c3617a4625aac0cf2ca5
honey0632 <honey603020@gmail.com>
2024-03-20 22:36:43 +0530

    mar 20

---
+++ 03-20-2024.cpp
@@ -0,0 +1,41 @@
+// 
+// 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(!root){return {};}
+        std::queue<TreeNode*> topq, botq;
+        topq.push(root);
+        std::vector<int> ans;
+        int curMax = INT_MIN;
+        while(!topq.empty()){
+            TreeNode *node = topq.front();
+            topq.pop();
+            curMax = max(curMax, node->val);
+            if(node->right){botq.push(node->right);}
+            if(node->left){botq.push(node->left);}
+            if(topq.empty()){
+                ans.push_back(curMax);
+                curMax = INT_MIN, topq = botq, botq = {};
+            }
+        }
+        return ans;
+    }
+};
+int main () {
+    
+     return 0;
+}
