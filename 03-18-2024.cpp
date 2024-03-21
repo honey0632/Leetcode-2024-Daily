85a8d082bad96e2afcb26d2f4d2ec5bb410bf5e8
honey0632 <honey603020@gmail.com>
2024-03-21 14:25:02 +0530

    mar 18

--- 03-18-2024.cpp
+++ 03-18-2024.cpp
@@ -1,60 +1,34 @@
-// 
-// 
+// 452. Minimum Number of Arrows to Burst Balloons
+// https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
 
 #include<bits/stdc++.h>
 using namespace std;
 
 
-// This is the interface that allows for creating nested lists.
-// You should not implement it, or speculate about its implementation
-class NestedInteger {
-  public:
-    // Return true if this NestedInteger holds a single integer, rather than a nested list.
-    bool isInteger() const;
-    // Return the single integer that this NestedInteger holds, if it holds a single integer
-    // The result is undefined if this NestedInteger holds a nested list
-    int getInteger() const;
-    // Return the nested list that this NestedInteger holds, if it holds a nested list
-    // The result is undefined if this NestedInteger holds a single integer
-    const vector<NestedInteger> &getList() const;
-};
-
-
-class NestedIterator {
+class Solution {
 public:
-    vector<int> flat;
-    int ind, size;
-    NestedIterator(vector<NestedInteger> &nestedList) {
-        makeFlat(nestedList);
-        this->ind = 0;
-        this->size = flat.size();
-    }
-
-    void makeFlat(vector<NestedInteger> &nestedList){
-        for(auto it : nestedList){
-            if(it.isInteger()){
-                this->flat.push_back(it.getInteger());
-            }else{
-                // makeFlat(it.getList());  uncomment it
+    int findMinArrowShots(vector<vector<int>>& a) {
+        int n=a.size(),ans=0;
+        sort(a.begin(),a.end());
+        int i=0,j=0;
+        while(i<n && j<n){
+            if(i==j){
+                ans++;
+                j++;
+            }
+            else if(a[i][1]<a[j][0]){
+                i++;
+            }
+            else if(a[i][1]>=a[j][0]){
+                a[j][1]=min(a[i][1],a[j][1]);
+                i=j;
+                j++;
+                
             }
         }
-    }
-    
-    int next() {
-        if(this->ind >= this->size) return NULL;
-        return this->flat[ind++];
-    }
-    
-    bool hasNext() {
-        return (this->ind < this->size);
+        return ans;
     }
 };
-
-/**
- * Your NestedIterator object will be instantiated and called as such:
- * NestedIterator i(nestedList);
- * while (i.hasNext()) cout << i.next();
- */
 int main () {
     
      return 0;
