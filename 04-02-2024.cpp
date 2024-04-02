4a683a32a4351b0bf84a00f48b519c677a6f6891
honey0632 <honey603020@gmail.com>
2024-04-02 19:17:26 +0530

    apr 02

---
+++ 04-02-2024.cpp
@@ -0,0 +1,36 @@
+// 205. Isomorphic Strings
+// https://leetcode.com/problems/isomorphic-strings/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    bool isIsomorphic(string s, string t) {
+    
+        vector<int> indexS(200, 0); 
+        vector<int> indexT(200, 0); 
+        
+        int len = s.length();
+
+        if(len != t.length()) {
+            return false;
+        }
+        
+        for(int i = 0; i < len; i++) {
+            if(indexS[s[i]] != indexT[t[i]]){
+                return false;
+            }
+            
+            indexS[s[i]] = i + 1; 
+            indexT[t[i]] = i + 1; 
+        }
+
+        return true;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
