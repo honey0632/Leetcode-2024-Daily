7a39cdb7ce8d3c7abb063984e5856177dfe49bf8
honey0632 <honey603020@gmail.com>
2024-04-19 09:13:27 +0530

    apr 19

---
+++ 04-19-2024.cpp
@@ -0,0 +1,39 @@
+// 200. Number of Islands
+// https://leetcode.com/problems/number-of-islands/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+     int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n =  grid[0].size(), islands = 0, offsets[] = {0, 1, 0, -1, 0};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> todo;
+                    todo.push({i, j});
+                    while (!todo.empty()) {
+                        pair<int, int> p = todo.front();
+                        todo.pop();
+                        for (int k = 0; k < 4; k++) {
+                            int r = p.first + offsets[k], c = p.second + offsets[k + 1];
+                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
+                                grid[r][c] = '0';
+                                todo.push({r, c});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return islands;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
