a062cf7498e9380c447879fc1db6c12e583b1c2a
honey0632 <honey603020@gmail.com>
2024-05-26 15:00:57 +0530

    may 26

---
+++ 05-26-2024.cpp
@@ -0,0 +1,44 @@
+// 552. Student Attendance Record II
+// https://leetcode.com/problems/student-attendance-record-ii/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    static const int MOD = 1000000000 + 7;
+
+public:
+    int checkRecord(int n) {
+        vector<vector<int>> prevDP(2, vector<int>(3, 1));
+
+        for (int i = 1; i <= n; i++) {
+            vector<vector<int>> newDP(2, vector<int>(3, 0));
+            for (int a = 0; a < 2; a++) {
+                for (int l = 0; l < 3; l++) {
+                    // Pick P
+                    newDP[a][l] += prevDP[a][2];
+                    newDP[a][l] %= MOD;
+                    if (a > 0) {
+                        // Pick A
+                        newDP[a][l] += prevDP[a - 1][2];
+                        newDP[a][l] %= MOD;
+                    }
+                    if (l > 0) {
+                        // Pick L
+                        newDP[a][l] += prevDP[a][l - 1];
+                        newDP[a][l] %= MOD;
+                    }
+                }
+            }
+            prevDP = newDP;
+        }
+
+        return prevDP[1][2];
+    }
+};
+
+int main () {
+    
+     return 0;
+}
