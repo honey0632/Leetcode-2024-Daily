3a9a26eceb19dec04a817cb2ddfd46fbb8b79e9c
honey0632 <honey603020@gmail.com>
2024-06-05 23:48:04 +0530

    jun 05

---
+++ 06-05-2024.cpp
@@ -0,0 +1,34 @@
+// 1002. Find Common Characters
+// https://leetcode.com/problems/find-common-characters/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> min_freq(26, INT_MAX);
+        for (auto& word : words) {
+            vector<int> freq(26, 0);
+            for (char c : word) {
+                freq[c - 'a']++;
+            }
+            for (int i = 0; i < 26; ++i) {
+                min_freq[i] = min(min_freq[i], freq[i]);
+            }
+        }
+
+        vector<string> result;
+        for (int i = 0; i < 26; ++i) {
+            while (min_freq[i]-- > 0) {
+                result.push_back(string(1, i + 'a'));
+            }
+        }
+        return result;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
