dfc3ccad90aa2deaae2aca1b9851cc514f7cffbf
honey0632 <honey603020@gmail.com>
2024-06-28 21:52:50 +0530

    jun 28

---
+++ 06-28-2024.cpp
@@ -0,0 +1,40 @@
+// 2285. Maximum Total Importance of Roads
+// https://leetcode.com/problems/maximum-total-importance-of-roads/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<int> degree(n, 0); // Array to store the degree of each city
+        
+        // Calculate the degree of each city
+        for (const auto& road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+        
+        // Create a list of cities and sort by degree
+        vector<int> cities(n);
+        for (int i = 0; i < n; i++) {
+            cities[i] = i;
+        }
+        sort(cities.begin(), cities.end(), [&](int a, int b) {
+            return degree[a] > degree[b];
+        });
+        
+        // Assign values to cities starting from the highest degree
+        long long totalImportance = 0;
+        for (int i = 0; i < n; i++) {
+            totalImportance += (long long)(n - i) * degree[cities[i]];
+        }
+        
+        return totalImportance;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
