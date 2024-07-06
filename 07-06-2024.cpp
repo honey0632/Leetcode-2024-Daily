b0b1732c3beaef3e511098597f6ab8d7afcdf5e6
honey0632 <honey603020@gmail.com>
2024-07-06 20:04:24 +0530

    jul 06

---
+++ 07-06-2024.cpp
@@ -0,0 +1,18 @@
+// 2582. Pass the Pillow
+// https://leetcode.com/problems/pass-the-pillow/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int chunks = time / (n - 1);
+        return chunks % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
+    }
+};
+
+int main () {
+    
+     return 0;
+}
