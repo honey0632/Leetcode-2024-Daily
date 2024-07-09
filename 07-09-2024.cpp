05fad61975b9f1311aa7dbded957afbdad3628a5
honey0632 <honey603020@gmail.com>
2024-07-09 19:26:06 +0530

    jul 09

---
+++ 07-09-2024.cpp
@@ -0,0 +1,31 @@
+// 1701. Average Waiting Time
+// https://leetcode.com/problems/average-waiting-time/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        double total_waiting_time = 0;
+        int current_time = 0;
+
+        for (const auto& customer : customers) {
+            int arrival = customer[0];
+            int service = customer[1];
+            if (current_time < arrival) {
+                current_time = arrival;
+            }
+            int waiting_time = current_time - arrival + service;
+            total_waiting_time += waiting_time;
+            current_time += service;
+        }
+
+        return total_waiting_time / customers.size();
+    }
+};
+
+int main () {
+    
+     return 0;
+}
