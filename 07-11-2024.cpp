a2a46f83fc50bedcbdea8ee2866a03fe3ae2b196
honey0632 <honey603020@gmail.com>
2024-07-11 22:25:02 +0530

    jul 11

---
+++ 07-11-2024.cpp
@@ -0,0 +1,48 @@
+// 1190. Reverse Substrings Between Each Pair of Parentheses
+// https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        int n = s.length();
+        vector<int> index_pairs(n, 0);
+        deque<int> stack_start_ind;
+
+        for (int char_ind = 0; char_ind < n; ++char_ind) {
+            char char_s = s[char_ind];
+            if (char_s == '(') {
+                stack_start_ind.push_back(char_ind);
+            } else if (char_s == ')') {
+                int start_ind = stack_start_ind.back();
+                stack_start_ind.pop_back();
+                index_pairs[char_ind] = start_ind;
+                index_pairs[start_ind] = char_ind;
+            }
+        }
+
+        string res;
+        int cur_ind = 0;
+        int cur_dir = 1;
+
+        while (cur_ind < n) {
+            char char_s = s[cur_ind];
+            if (char_s == '(' || char_s == ')') {
+                cur_ind = index_pairs[cur_ind];
+                cur_dir *= -1;
+            } else {
+                res.push_back(char_s);
+            }
+            cur_ind += cur_dir;
+        }
+
+        return res;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
