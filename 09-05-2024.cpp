947602f651a5db83d0e7e0b24dda977aab0d6afe
honey0632 <honey603020@gmail.com>
2024-09-05 23:10:51 +0530

    sep 05

---
+++ 09-05-2024.cpp
@@ -0,0 +1,26 @@
+// 2028. Find Missing Observations
+// https://leetcode.com/problems/find-missing-observations/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+    int curSum = accumulate(rolls.begin(), rolls.end(), 0), m = rolls.size();
+    
+    int missingSum = mean * (n + m) - curSum;
+    if (missingSum < n || missingSum > 6*n) return {};
+    
+    int part = missingSum / n, rem = missingSum % n;
+    vector<int> ans(n, part);
+    for (int i = 0; i < rem; ++i) 
+        ++ans[i];
+    return ans;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
