d68e3fea2ff853a8d46d035db2fdedf357db150b
honey0632 <honey603020@gmail.com>
2024-09-14 23:44:53 +0530

    sep 14

--- 09-14-2024.cpp
+++ 09-14-2024.cpp
@@ -9,6 +9,7 @@ public:
     int longestSubarray(vector<int>& nums) {
         int maxi=*max_element(nums.begin(),nums.end());
         int temp_length=0;
+        
         int ans=1;
 
         for(int i=0;i<nums.size();i++){
