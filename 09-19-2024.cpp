28bdb313c92bea579da1bb4e479fa420208269ae
honey0632 <honey603020@gmail.com>
2024-09-19 18:13:17 +0530

    sep 19

---
+++ 09-19-2024.cpp
@@ -0,0 +1,40 @@
+// 241. Different Ways to Add Parentheses
+// https://leetcode.com/problems/different-ways-to-add-parentheses/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        vector<int> res;  // equal to this { } symbol functionality
+        for(int i=0; i<input.length(); i++){
+            if(input[i]=='+'||input[i]=='-'||input[i]=='*'){
+                vector<int> left = diffWaysToCompute(input.substr(0,i));    //divide to two parts 
+                vector<int> right = diffWaysToCompute(input.substr(i+1));
+                for(int j=0; j<left.size(); j++){
+                    for(int k=0; k<right.size(); k++){
+                        if(input[i] == '+'){
+                            res.push_back(left[j]+right[k]);
+                        }
+                        else if(input[i] == '-'){
+                            res.push_back(left[j]-right[k]);
+                        }
+                        else{
+                            res.push_back(left[j]*right[k]);
+                        }
+                    }
+                }
+            }
+        }
+        if(res.empty()){  //if input only has one number character ex. f(5)
+            res.push_back(stoi(input));
+        }
+        return res;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
