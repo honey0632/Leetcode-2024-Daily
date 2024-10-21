4ce879e8123ff4a2393a4630a765ad1ac3756d02
honey0632 <honey603020@gmail.com>
2024-10-21 17:10:38 +0530

    oct 21

---
+++ 10-21-2024.cpp
@@ -0,0 +1,34 @@
+// 1593. Split a String Into the Max Number of Unique Substrings
+// https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    int backtrack(int start, const string& s, unordered_set<string>& seen) {
+        if (start == s.size()) {
+            return 0;
+        }
+        int maxSplits = 0;
+        for (int end = start + 1; end <= s.size(); ++end) {
+            string substring = s.substr(start, end - start);
+            if (seen.find(substring) == seen.end()) {
+                seen.insert(substring);
+                maxSplits = max(maxSplits, 1 + backtrack(end, s, seen));
+                seen.erase(substring);
+            }
+        }
+        return maxSplits;
+    }
+    int maxUniqueSplit(string s) {
+        unordered_set<string> seen;
+        return backtrack(0, s, seen);
+    }
+};
+
+int main() {
+    
+     return 0;
+}
