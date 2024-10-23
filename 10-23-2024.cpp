62a735eb0a3b8c2324662148a4aafef6fd0f9a53
honey0632 <honey603020@gmail.com>
2024-10-23 17:55:44 +0530

    oct 23

---
+++ 10-23-2024.cpp
@@ -0,0 +1,60 @@
+// 2641. Cousins in Binary Tree II
+// https://leetcode.com/problems/cousins-in-binary-tree-ii/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        if (!root) return nullptr;
+
+        queue<TreeNode*> q;
+        int prev = root->val;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            int curr = 0;
+
+            while (size > 0) {
+                TreeNode* temp = q.front();
+                q.pop();
+
+                int leftRight = (temp->left ? temp->left->val : 0) + (temp->right ? temp->right->val : 0);
+
+                if (temp->left) {
+                    temp->left->val = leftRight;
+                    q.push(temp->left);
+                }
+
+                if (temp->right) {
+                    temp->right->val = leftRight;
+                    q.push(temp->right);
+                }
+
+                curr += leftRight;
+                temp->val = prev - temp->val;
+                size--;
+            }
+
+            prev = curr;
+        }
+
+        return root;
+    }
+};
+
+int main() {
+    
+     return 0;
+}
