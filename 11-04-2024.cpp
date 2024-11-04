d6ff32dd51cdefdc264de304829a5dc0427e0f2d
honey0632 <honey603020@gmail.com>
2024-11-04 17:11:04 +0530

    nov 04

---
+++ 11-04-2024.cpp
@@ -0,0 +1,36 @@
+// 3163. String Compression III
+// https://leetcode.com/problems/string-compression-iii/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    static string compressedString(string& word) {
+        int n=word.size(), l=0;
+        string ans;
+        ans.reserve(n);
+        for(int r=0; r<n; ){
+            while( r<n && word[r]==word[l]) r++;
+
+            auto [q, rem]=div(r-l, 9);
+            
+            for(int i=0; i<q; i++){
+                ans.push_back('9');
+                ans.push_back(word[l]);
+            }
+            if (rem>0) {
+                ans.push_back(rem+'0');
+                ans.push_back(word[l]);
+            }
+            l=r;
+        }
+        return ans;
+    }
+};
+
+
+int main() {
+    
+     return 0;
+}
