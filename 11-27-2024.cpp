b5778b1364bbe448e978d5af9b4662f412b8f30f
honey0632 <honey603020@gmail.com>
2024-11-27 21:18:02 +0530

    nov 27

---
+++ 11-27-2024.cpp
@@ -0,0 +1,58 @@
+// 3243. Shortest Distance After Road Addition Queries I
+// https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> shortestDistanceAfterQueries(int n,
+                                             vector<vector<int>>& queries) {
+
+        vector<int> ans;
+        vector<vector<int>> adj(n);
+        vector<int> dist(n);
+
+        iota(dist.begin(), dist.end(), 0); //fills the array with 0,1,2,3...
+        for (int i = 0; i < n - 1; i++) {
+            adj[i].push_back(i + 1);
+        }
+
+        
+        for (auto it : queries) {
+
+            int src = it[0], des = it[1];
+            adj[src].push_back(des);
+
+            if (dist[src] + 1 < dist[des]) {
+                //BFS
+                queue<int> q;
+                q.push(des);
+                dist[des] = dist[src] + 1;
+
+                while (q.size()) {
+
+                    int idx = q.front();
+                    q.pop();
+
+                    for (auto e : adj[idx]) {
+
+                        if (dist[idx] + 1 < dist[e]) {
+                            dist[e] = dist[idx] + 1;
+                            q.push(e);
+                        }
+
+                    }
+                }
+            }
+
+            ans.emplace_back(dist.back());
+        }
+        return ans;
+    }
+};
+
+int main() {
+    
+     return 0;
+}
