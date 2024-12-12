ca2b3c6e721cf6d4c37df9f5bb6e7378974ef62d
honey0632 <honey603020@gmail.com>
2024-12-12 21:11:02 +0530

    dec 12

---
+++ 12-12-2024.cpp
@@ -0,0 +1,29 @@
+// 2558. Take Gifts From the Richest Pile
+// https://leetcode.com/problems/take-gifts-from-the-richest-pile/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> q;
+        long long ans = 0;
+        for (int i : gifts) {
+            q.push(i);
+            ans += i;
+        }
+        while (k--) {
+            int v = q.top();
+            ans -= (v - sqrt(v));
+            q.pop();
+            q.push(sqrt(v));
+        }
+        return ans;
+    }
+};
+
+int main() {
+    
+     return 0;
+}
