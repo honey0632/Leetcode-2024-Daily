7da978d2120ca98e910ac9527c506043019c181f
honey0632 <honey603020@gmail.com>
2024-12-26 23:15:38 +0530

    dec 26

--- 12-25-2024.cpp
+++ 12-25-2024.cpp
@@ -1,1 +1,43 @@
-12-25-2024.
+// 515. Find Largest Value in Each Tree Row
+// https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(!root){return {};}
+        queue<TreeNode*> topq, botq;
+        topq.push(root);
+        vector<int> ans;
+        int curMax = INT_MIN;
+        while(!topq.empty()){
+            TreeNode *node = topq.front();
+            topq.pop();
+            curMax = max(curMax, node->val);
+            if(node->right){botq.push(node->right);}
+            if(node->left){botq.push(node->left);}
+            if(topq.empty()){
+                ans.push_back(curMax);
+                curMax = INT_MIN, topq = botq, botq = {};
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    
+     return 0;
+}
